9ab0d75136333239d6059f75731076d0a9388e98
sathish kumar <149255877+sathishnota@users.noreply.github.com>
2025-05-29 10:59:17 +0530

    Add files via upload
    Day 2:
    Challenge : Different Data Types & Area of rectangle using User-input.

---
+++ variable.py
@@ -0,0 +1,5 @@
+#Calculate the area of a rectangle using user-input length and width
+length = int(input("Enter the length: "))
+width = int(input("enter the width: "))
+area = length * width
+print(f"Area of a rectangle is {area}")
